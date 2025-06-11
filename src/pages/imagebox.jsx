aedcc54473a6c9f7c450c97c73a7a49d8998b7e7
Abdallah199510 <abdelfattahabdallah608@gmail.com>
2025-06-11 23:20:56 +0300

    first commit

--- src/pages/imagebox.jsx
+++ src/pages/imagebox.jsx
@@ -4,7 +4,7 @@ const ImageBox = ({image}) => {
   return (
     <div className="image-box" >
       <img src={image} alt="Project" />
-      <button className="custom-button">View Project</button>
+      <button className="custom-button text-center">View Project</button>
     </div>
   );
 };
